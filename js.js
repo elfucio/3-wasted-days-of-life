93e5981514f36e215959896cc3434afdd7d9be0c
Elya <elfucio@gmail.com>
2020-01-28 00:36:59 +0300

    getting better

--- js.js
+++ js.js
@@ -1,27 +1,12 @@
 'use strict';
 
 let btn = document.querySelector ('#btn'),
-    total = document.querySelector ('.total'),
-    toppings = [];
+    total = document.querySelector ('.total');
       
 btn.addEventListener('click', function() {
   let burger = new Burger ('size', 'stuffing', 'extra');
-  toppings.push(burger);
+  console.log(burger);
 });
-
-
-
-// class Hamburger {
-//     constructor(size, stuffing) { ... }
-//     addTopping(topping) {    // Добавить добавку }
-//     removeTopping(topping) { // Убрать добавку }
-//     getToppings(topping) {   // Получить список добавок }
-//     getSize() {              // Узнать размер гамбургера }
-//     getStuffing() {          // Узнать начинку гамбургера }
-//     calculatePrice() {       // Узнать цену }
-//     calculateCalories() {    // Узнать калорийность }
-//   };
-// };
 
 class Burger {
     constructor (size, stuffing, extra) {
@@ -49,55 +34,9 @@ class Burger {
           arr.push (el.value);
       });
       return arr;
-    };
-
-    showOrder () {
-      this.total.innerText = `Итого: ${this.price} и ${this.calories}`;
-    }
-
-
-
-
-
-
-
-
-
-
-
-      
+    };      
 };
 
-// class Human {
-//     constructor (nam, ag, gen) {
-//         this.name = nam
-//         this.age = ag
-//         this.gender = gen ? 'male' : 'female'
-//         this.married = false
-//         this.partner = null
-//     }
-//     goWork () {
-//         console.log (`${this.name} goes work.`)
-//     }
-//     getMarried (pers) {
-//         if (!this.married && !pers.married) {
-//             this.married = true
-//             this.partner = pers
-//             pers.getMarried (this)
-//         } else {
-//             console.log (`${this.name} is already married with ${this.partner.name}`)
-//         }
-//     }
-// }
-
-// let oleg = new Human ('Oleg', 23, 1)
-// let sopha = new Human ('Sophia', 22, 0)
-
-// class NewClass {
-//     constructor () {
-//         this.prop = 100
-//         this.prop2 = 200
-//         this.method ()
-//     }
-//     method () {
-//         console.log (123)
+showOrder () {
+  this.total.innerText = `Итого: ${this.price} рублей и ${this.calories} калорий`;
+};
