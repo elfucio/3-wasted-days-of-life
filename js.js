c800dd0ca4ee6bfe3655ca42343b59d4e2f93ade
Elya <elfucio@gmail.com>
2020-01-29 19:24:34 +0300

    i am stupid

--- js.js
+++ js.js
@@ -1,50 +1,61 @@
 'use strict';
 
 let btn = document.querySelector ('#btn'),
+    uForm = document.querySelector('#uForm'),
     total = document.querySelector ('.total');
       
 btn.addEventListener('click', function() {
-  let burger = new Burger ('price', 'calories','size', 'stuffing', 'extra');
+  let burger = new Burger ('price', 'calories', 'size', 'stuffing', 'extra');
   console.log(burger);
 });
 
 class Burger {
     constructor (price, calories, size, stuffing, extra) {
-      this.price = this.getPrice;
-      this.calories = 0;
-       this.size = this._getSize(size);
+      this.price = this.getPrice(price);
+      this.calories = this.getCalories(calories);
+      this.size = this._getSize(size);
       this.stuffing = this._getStuffing(stuffing);
       this.extra = this._getExtra(extra);
     };
 
     getPrice(attrName) {
-      let obj = document.querySelector(`input[data=${attrName}]:checked`);
-      console.log(obj);
-      return obj.getAttribute('data-price');
-    };    
+      let obj = {};
+      obj = uForm.querySelector (`input[name=${attrName}]:checked`);      
+      return obj;
+    };
+
+    getCalories(attrName) {
+      let obj = {};
+      obj = uForm.querySelector (`input[name=${attrName}]:checked`);      
+      return obj;
+    };
 
     _getSize(attrName) {
-      let obj = document.querySelector (`input[name=${attrName}]:checked`);
-      return obj.value;
+      let obj = {};
+      obj = uForm.querySelector (`input[name=${attrName}]:checked`);
+      console.log(obj.dataset.price);
+      console.log(obj.dataset.calories);
+      return obj.dataset;      
     };
 
     _getStuffing(attrName) {
-      let obj = document.querySelector (`input[name=${attrName}]:checked`);
-      return obj.value;
+      let obj = {};
+      obj = uForm.querySelector (`input[name=${attrName}]:checked`);     
+      console.log(obj.dataset.price);
+      console.log(obj.dataset.calories); 
+      return obj.dataset;
     };
 
     _getExtra (attrName) {
-      let objArr = [...document.querySelectorAll (`input[name=${attrName}]:checked`)];
-      let arr = [];
-      objArr.forEach (el => {
-          arr.push (el.value);
-      });
-      return arr;
-    };  
-    // _getCalories (attrName) {
-    //   let obj = document.querySelector (`input[name=${attrName}]:checked`);
-    //   return obj.dataset['cal'];
-    // };    
+      let objects = uForm.querySelectorAll (`input[name=${attrName}]:checked`);
+      console.log(objects.dataset);
+      
+      // let arr = [];
+      // objArr.forEach (el => {
+      //     arr.push ([el.dataset("data-price"), el.dataset("data-cal")]);
+      // });
+      // return arr;
+    };
     
 };
 
