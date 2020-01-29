e3dc52105874d3c88860abf2d39023c0a899444d
Elya <elfucio@gmail.com>
2020-01-29 12:37:56 +0300

    please penguin god save me

--- js.js
+++ js.js
@@ -4,18 +4,24 @@ let btn = document.querySelector ('#btn'),
     total = document.querySelector ('.total');
       
 btn.addEventListener('click', function() {
-  let burger = new Burger ('size', 'stuffing', 'extra');
+  let burger = new Burger ('price', 'calories','size', 'stuffing', 'extra');
   console.log(burger);
 });
 
 class Burger {
-    constructor (size, stuffing, extra) {
-      this.price = 0;
+    constructor (price, calories, size, stuffing, extra) {
+      this.price = this.getPrice;
       this.calories = 0;
-      this.size = this._getSize(size);
+       this.size = this._getSize(size);
       this.stuffing = this._getStuffing(stuffing);
       this.extra = this._getExtra(extra);
     };
+
+    getPrice(attrName) {
+      let obj = document.querySelector(`input[data=${attrName}]:checked`);
+      console.log(obj);
+      return obj.getAttribute('data-price');
+    };    
 
     _getSize(attrName) {
       let obj = document.querySelector (`input[name=${attrName}]:checked`);
@@ -34,8 +40,15 @@ class Burger {
           arr.push (el.value);
       });
       return arr;
-    };      
+    };  
+    // _getCalories (attrName) {
+    //   let obj = document.querySelector (`input[name=${attrName}]:checked`);
+    //   return obj.dataset['cal'];
+    // };    
+    
 };
+
+
 
 // showTotal () {
 //   this.total.innerText = `Итого: ${this.price} рублей и ${this.calories} калорий`;
