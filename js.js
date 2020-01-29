1e5260ab9df57e88585b04d365f480a90f05bcd4
Elya <elfucio@gmail.com>
2020-01-30 00:01:55 +0300

    half works

--- js.js
+++ js.js
@@ -2,12 +2,11 @@
 
 let uForm = document.querySelector('#uForm'),
     btn = document.querySelector ('#btn'),
-    total = uForm.querySelector ('.total'),
-    choice = uForm.querySelector ('.choice');
+    total = document.querySelector ('.total');
       
 btn.addEventListener('click', function() {
   let burger = new Burger ('price', 'calories', 'size', 'stuffing', 'extra');
-  // burger.showOrder();
+  burger.showOrder();
   console.log(burger);
 });
 
@@ -18,31 +17,31 @@ class Burger {
       this.size = this._getSize(size);
       this.stuffing = this._getStuffing(stuffing);
       this.extra = this._getExtra(extra);
-    };
+    }
 
     _getPrice(attrName) {
       let obj = {};
       obj = uForm.querySelector (`input[name=${attrName}]:checked`);      
       return obj;
-    };
+    }
 
     _getCalories(attrName) {
       let obj = {};
       obj = uForm.querySelector (`input[name=${attrName}]:checked`);      
       return obj;
-    };
+    }
 
     _getSize(attrName) {
       let obj = {};
       obj = uForm.querySelector (`input[name=${attrName}]:checked`);
       return obj.dataset;      
-    };
+    }
 
     _getStuffing(attrName) {
       let obj = {};
       obj = uForm.querySelector (`input[name=${attrName}]:checked`);      
       return obj.dataset;
-    };
+    }
 
     _getExtra (attrName) {
       let objArr = [...uForm.querySelectorAll (`input[name=${attrName}]:checked`)];
@@ -51,14 +50,19 @@ class Burger {
           arr.push ([el.value, el.dataset.price, el.dataset.calories]);
       });
       return arr;
-    };
+    }
 
     calculateOrder() {		
       this.price = +this.size.price + +this.stuffing.price;
       this.calories = +this.size.calories + +this.stuffing.calories;
-      // this.extra.forEach (el => {
-      //   this.price = this.price + el.dataset.price;
-      // )};
-    };
+      this.extra.forEach (element => {
+        this.price += element.dataset.price;
+        this.calories += element.dataset.calories;
+      });
+    }
 
-};
+    showOrder () {		
+      this.calculateOrder();
+        total.innerText = `Итого: ${this.price} рублей, ${this.calories} калорий`;
+    }
+}
